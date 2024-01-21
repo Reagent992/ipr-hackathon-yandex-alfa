acd4f916d287c76c6689b29e4b62ac0c5366b395
Михаил Волков <green_panda@inbox.ru>
2024-01-21 16:36:49 +0300

    Исправил комментарии тим-лида v2

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env.str("SECRET_KEY")
 
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
