e80e0fd3c745bedcf7676668a5e825ce850f8281
Mikhail Volkov <61934932+greenpandorik@users.noreply.github.com>
2024-01-21 20:33:19 +0300

    Update models.py
    Убрал лишнее в  "status"

--- tasks/models.py
+++ tasks/models.py
@@ -52,7 +52,7 @@ class Task(models.Model):
     end_date = models.DateField(verbose_name="Дедлайн задачи",  null=True,
                                 blank=True,)
     status = models.CharField(
-        max_length=20, choices=TaskStatus.choices,
+        max_length=20, choices=TaskStatus,
         default=TaskStatus.NONE, verbose_name="Статус задачи"
     )
     skill = models.ManyToManyField(
