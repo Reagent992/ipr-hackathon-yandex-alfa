1a94c94681d6ac89f3280f7af0a89ae1d707f8d4
Miron Sadykov <reagent992@gmail.com>
2024-01-25 00:26:35 +0300

    stage commit

--- core/signals.py
+++ core/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from notifications.signals import notify
 
+from comments.models import Comment
 from ipr.models import IPR
 from tasks.models import Task
 
@@ -9,21 +10,31 @@ from tasks.models import Task
 @receiver(post_save, sender=IPR)
 def created_ipr_notification(sender, instance: IPR, created, **kwargs):
     """Создание уведомления о новом ИПР."""
-    # TODO: Уведомление о просьбе оценить ИПР, после его закрытия.
-    # TODO: Уведомление об изменение статуса ИПР.
     if created:
         text = "Вам назначен новый ИПР"
         notify.send(sender=instance, recipient=instance.executor, verb=text)
+    else:
+        text = "Изменение в ИПР."
+        notify.send(sender=instance, recipient=instance.executor, verb=text)
 
 
-# TODO: сигнал при создание задачи
-@receiver(post_save, sender=IPR)
+@receiver(post_save, sender=Task)
 def created_task_notification(sender, instance: Task, created, **kwargs):
     """Создание уведомления о новой задачи."""
 
     if created:
-        text = "Вам добавили новую задачу"
+        text = "Вам добавлена новая задача"
+        notify.send(sender=instance, recipient=instance.executor, verb=text)
+    else:
+        text = "Изменение в задаче."
         notify.send(sender=instance, recipient=instance.executor, verb=text)
 
 
-# TODO: сигнал при создание комментария
+@receiver(post_save, sender=Comment)
+def created_comment_notification(sender, instance: Comment, created, **kwargs):
+    """Создание уведомления о новом комментарии."""
+    if created:
+        text = "Новый комментарий"
+        notify.send(
+            sender=instance, recipient=instance.task.executor, verb=text
+        )
