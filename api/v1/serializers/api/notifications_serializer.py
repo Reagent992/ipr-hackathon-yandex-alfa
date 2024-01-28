634de229c7554f038d4a2fb1d650300b42969602
Miron Sadykov <reagent992@gmail.com>
2024-01-28 15:16:03 +0300

    notification serializer improvement

--- api/v1/serializers/api/notifications_serializer.py
+++ api/v1/serializers/api/notifications_serializer.py
@@ -1,14 +1,14 @@
 from notifications.models import Notification
 from rest_framework import serializers
 
-from api.v1.serializers.api.users_serializer import CustomUserSerializer
-
 
 class NotificationSerializer(serializers.ModelSerializer):
     """Сериализатор уведомлений."""
 
-    recipient = CustomUserSerializer(read_only=True, many=False)
-    actor = CustomUserSerializer(read_only=True, many=False)
+    recipient_id = serializers.IntegerField(
+        read_only=True, source="recipient.id"
+    )
+    actor_id = serializers.IntegerField(read_only=True, source="actor.id")
     target_object_id = serializers.IntegerField(read_only=True)
     target_content_type = serializers.CharField(
         source="target.__class__.__name__", read_only=True
@@ -23,15 +23,6 @@ class NotificationSerializer(serializers.ModelSerializer):
             "target_object_id",
             "target_content_type",
             "timestamp",
-            "recipient",
-            "actor",
+            "recipient_id",
+            "actor_id",
         )
-
-    # TODO: Добавить ссылку на объект уведомления.
-    # url = reverse('your_nested_detail_view_name',
-    #     kwargs={'content_type': contentType, 'pk': objectId})
-    # >>> reverse("users-detail", args=[user.id])
-    # >>> '/api/v1/users/5/'
-    # request.build_absolute_uri('/some-relative-path/')
-    # т.е. всего 3 варианта, можно if-ами пройти.
-    # if isinstance(notification.target, IPR):
