c071e96f76490faead3359a94d66fcd8feea2041
Артур Галиаскаров <118313269+Arti1946@users.noreply.github.com>
2024-01-31 15:57:55 +0300

    API for IPR (#38)
    * Create api files for IPR
    
    * Небольшой ренейминг
    
    * Fix for ipr serializers, views, model and fixture
    
    * Fix IPR model and some other improvements
    
    * fix serializer
    
    * fix filtering
    
    ---------
    
    Co-authored-by: Miron Sadykov <reagent992@gmail.com>

---
+++ api/v1/views/ipr_views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from api.v1.filters import IPRFilter
+from api.v1.permissions import TeamBossPermission
+from api.v1.serializers.api.ipr_serializers import (
+    IPRSerializer,
+    IPRSerializerPost,
+)
+from core.statuses import Status
+from ipr.models import IPR
+from users.models import User
+
+
+class IPRViewSet(ModelViewSet):
+    serializer_class = IPRSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = IPRFilter
+    http_method_names = ["get", "post", "patch", "head", "options"]
+
+    def get_queryset(self):
+        if self.request.query_params:
+            return IPR.objects.filter(
+                executor=self.request.query_params.get("user_id")
+            )
+        return IPR.objects.filter(executor=self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method in ("POST", "PATCH"):
+            return IPRSerializerPost
+        return IPRSerializer
+
+    def perform_create(self, serializer):
+        executor_id = self.request.query_params.get("user_id")
+        executor = get_object_or_404(User, id=executor_id)
+        serializer.save(creator=self.request.user, executor=executor)
+
+    def get_permissions(self):
+        if self.request.method not in permissions.SAFE_METHODS:
+            self.permission_classes = [TeamBossPermission]
+        return super(IPRViewSet, self).get_permissions()
+
+    @action(
+        detail=True,
+        methods=["get"],
+    )
+    def status(self, request, pk):
+        """Дополнительный эндпоинт и View-функция
+        для отображения прогресса выполнения ИПР"""
+        ipr = get_object_or_404(IPR, id=pk)
+        tasks_without_trail = ipr.tasks.exclude(status=Status.TRAIL).count()
+        tasks_is_complete = ipr.tasks.filter(status=Status.COMPLETE).count()
+        progress = tasks_without_trail / 100 * tasks_is_complete
+
+        context = {
+            "request": request,
+            "progress": progress,
+        }
+        serializer = IPRSerializer(ipr, context=context)
+        return Response(serializer.data)
--- ipr/models.py
+++ ipr/models.py
@@ -1,21 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-User = get_user_model()
+from core.statuses import Status
 
-STATUSES = [
-    ("STATUS_ABSENT", "Отсутствует"),
-    ("STATUS_COMPLETED", "Выполнен"),
-    ("STATUS_NOT_COMPLETED", "Не выполнен"),
-    ("STATUS_IN_PROGRESS", "В работе"),
-    ("STATUS_CANCELLED", "Отменен"),
-    ("STATUS_DELAYED", "Отстает"),
-]
+User = get_user_model()
 
 
 class IPR(models.Model):
     title = models.CharField("Название ИПР", max_length=100)
-    description = models.CharField("Описание ИПР", max_length=500)
     creator = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -25,18 +17,14 @@ class IPR(models.Model):
     creation_date = models.DateField("Дата создания ИПР", auto_now_add=True)
     start_date = models.DateField("Дата начала работ по ИПР")
     end_date = models.DateField("Дедлайн ИПР")
-    status = models.CharField("Статус ИПР", max_length=20, choices=STATUSES)
+    status = models.CharField(
+        "Статус ИПР", max_length=20, choices=Status, default=Status.IN_PROGRESS
+    )
     executor = models.ForeignKey(
         User,
         verbose_name="Исполнитель ИПР",
         on_delete=models.CASCADE,
         related_name="ipr",
-    )
-    usability = models.PositiveSmallIntegerField(
-        verbose_name="Удобство использования ИПР", default=0
-    )
-    ease_of_creation = models.PositiveSmallIntegerField(
-        verbose_name="Удобство создания ИПР", default=0
     )
 
     class Meta:
