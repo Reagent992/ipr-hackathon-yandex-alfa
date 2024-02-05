45aa0dab478583f1b4dea6a1e006572bdefad8a0
greenpandorik <green_panda@inbox.ru>
2024-02-05 10:58:38 +0300

    Fix patch for user->user change status v1

--- api/v1/permissions.py
+++ api/v1/permissions.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions
 
+from tasks.models import Task
+
 User = get_user_model()
 
 
@@ -23,10 +25,11 @@ class TeamBossPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user == obj.creator:
             return True
-        if (
-            request.method == "PATCH"
-            and "status" in request.data
-            and request.data["status"] == "complete"
-        ):
-            return request.user == obj.executor
+        if isinstance(obj, Task):
+            if (
+                request.method == "PATCH"
+                and "status" in request.data
+                and request.data["status"] == "complete"
+            ):
+                return request.user == obj.executor
         return False
